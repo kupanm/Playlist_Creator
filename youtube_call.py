50b876fbf73871dcfd60a780052c84ceb77b6673
Kupa <kupakwashenm@gmail.com>
2024-08-27 21:22:30 +1000

    made a basic api call

--- youtube_call.py
+++ youtube_call.py
@@ -6,8 +6,22 @@
 5: Add the song to the Spotify Playlist
 """
 
+from googleapiclient.discovery import build
 import os
 
-api_key = os.environ.get("YT_API_KEY")
 
+
+api_key = os.environ.get("YT_API_KEY")
 print(api_key)
+
+
+youtube = build('youtube',
+                'v3',
+                developerKey=api_key)
+
+request = youtube.channels().list(
+    part='statistics',
+    forUsername='schafer5'
+)
+
+request = request.execute()
