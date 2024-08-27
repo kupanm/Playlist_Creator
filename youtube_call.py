29f7eb3c542f48747336daf456097e943ab69339
Kupa <kupakwashenm@gmail.com>
2024-08-27 20:50:12 +1000

    Added the steps to create API

--- youtube_call.py
+++ youtube_call.py
@@ -1,3 +1,11 @@
+"""
+1: Log Into Youtube
+2: Grab Liked Videos (possibly if type music ID (10))
+3: Create a New Playlist
+4: Search for the song
+5: Add the song to the Spotify Playlist
+"""
+
 import os
 
 api_key = os.environ.get("YT_API_KEY")
