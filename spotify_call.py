ad789acf96022a43856ca803a84b96eab15fe68c
Kupa <kupakwashenm@gmail.com>
2024-08-31 19:03:48 +1000

    Added client id and client secret file for spotify call. Wrote a basic api call. Not working right now due to an unknown error

--- spotify_call.py
+++ spotify_call.py
@@ -3,20 +3,40 @@
 5: Add the song to the Spotify Playlist
 
 """
+import json
 import os
+from dotenv import load_dotenv
+import base64
+from requests import post
 
+load_dotenv()
 
-client_id = os.environ.get("SPOTIFY_CLIENT_ID")
+client_id = os.getenv("CLIENT_ID")
 
 if not client_id:
     raise ValueError("No client ID found")
 
-client_secret = os.environ.get("SPOTIFY_CLIENT_SECRET")
+client_secret = os.getenv("CLIENT_SECRET")
 
 if not client_secret:
     raise ValueError("No client secret found")
 
+def get_token():
+    auth_str = client_id + ":" + client_secret
+    auth_bytes = auth_str.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+    print(auth_base64)
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic" + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    # token = json_result["access_token"]
+    return json_result
 
-# print(client_id)
-
-# print(client_secret)
+token = get_token()
+print(token)
+    
