c854a4a64aca1e0790ee98573854de48fcee3b29
Kupa <kupakwashenm@gmail.com>
2024-09-01 13:51:00 +1000

    Added a successful spotify oauth call.

--- spotify_call.py
+++ spotify_call.py
@@ -1,42 +1,80 @@
 """
-4: Search for the song
-5: Add the song to the Spotify Playlist
-
+    1. Get all playlists
+    2. Create a youtube liked music playlist (if it doesn't exist)
+    3. Search for the songs that the youtube script returns
+    4. Add those songs to the playlist
 """
-import json
+
+import time
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, request, url_for, session, redirect
 import os
 from dotenv import load_dotenv
-import base64
-from requests import post
 
 load_dotenv()
 
-client_id = os.getenv("CLIENT_ID")
-
+client_id = os.getenv('CLIENT_ID')
 if not client_id:
-    raise ValueError("No client ID found")
+    raise ValueError("NO CLIENT ID")
 
-client_secret = os.getenv("CLIENT_SECRET")
-
+client_secret = os.getenv('CLIENT_SECRET')
 if not client_secret:
-    raise ValueError("No client secret found")
+    raise ValueError("NO CLIENT SECRET")
+
+app_secret = os.getenv('APP_SECRET')
+if not app_secret:
+    raise ValueError("NO APP SECRET")
+
+
+app = Flask(__name__)
+
+app.config['SESSION_COOKIE_NAME'] = 'Spotify Cookie'
+app.secret_key = app_secret
+TOKEN_INFO = 'token_info'
+
+@app.route('/')
+def login():
+    auth_url = create_spotify_oauth().get_authorize_url()
+    return redirect(auth_url)
+
+
+@app.route('/redirect')
+def redirect_page():
+    session.clear()
+    code = request.args.get('code')
+    token_info = create_spotify_oauth().get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for('save_youtube_songs', external=True))
+
+
+@app.route('/saveYoutubeSongs')
+def save_youtube_songs():
+    try:
+        token_info = get_token()
+    except:
+        print("USER NOT LOGGED IN")
+        return redirect('/')
+    return("OAUTH SUCCESSFUL")
 
 def get_token():
-    auth_str = client_id + ":" + client_secret
-    auth_bytes = auth_str.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-    print(auth_base64)
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic" + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    # token = json_result["access_token"]
-    return json_result
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        redirect(url_for('login'), external=False)
+    now = int(time.time())
 
-token = get_token()
-print(token)
-    
+    is_expired = token_info['expires_at'] - now < 60
+    if(is_expired):
+        spotify_oauth = create_spotify_oauth()
+        token_info = spotify_oauth.refresh_access_token(token_info['refresh_token'])
+    return token_info
+
+
+def create_spotify_oauth():
+    return SpotifyOAuth(client_id=client_id,
+                        client_secret=client_secret,
+                        redirect_uri=url_for('redirect_page', _external=True),
+                        scope='user-library-read playlist-modify-public playlist-modify-private')
+
+
+app.run(debug=True)
